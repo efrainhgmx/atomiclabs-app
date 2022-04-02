e98358fb70b977427b2ec57d92713e2eef68d476
Efrain Hernandez <efrainhg2050@gmail.com>
2022-04-02 16:43:27 -0600

    Adding react router configuration and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Home = lazy(() => import('./components/views/Home'));
 
 function App() {
   return (
-    <div className="App">
-      HOLA
-    </div>
+   <Suspense fallback="Loading">
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+     </BrowserRouter>
+   </Suspense>
   );
 }
 
