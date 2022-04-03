33e7b93c82772d2a924463f35d6d152f6db5442d
Efrain Hernandez <efrainhg2050@gmail.com>
2022-04-03 15:31:12 -0500

    Second section in form

--- src/components/organisms/Form/Form.jsx
+++ src/components/organisms/Form/Form.jsx
@@ -11,9 +11,14 @@ const Form = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
   const [progress, setProgress] = useState(25);
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    if(progress >= 100) setProgress(100); 
+    setCounter(prevState => prevState + 1);
+    if(progress >= 100) {
+        setProgress(100);
+        setCounter(4)
+    } 
   }, [progress])
 
   return (
@@ -47,22 +52,66 @@ const Form = () => {
 
         <div className='title'>
         <span className='number active'>
-            <p>1</p>
+            <p>{counter}</p>
         </span>
-        <h2> TE QUEREMOS <span className='orange'>CONOCER</span></h2>
+        {(counter === 1) &&<h2> TE QUEREMOS <span className='orange'>CONOCER</span></h2>}
+        {(counter === 2) &&<h2> VALIDA TU <span className='orange'>CELULAR</span></h2>}
+        {(counter === 3) &&<h2> CÓDIGO DE <span className='orange'>VERIFICACIÓN</span></h2>}
+        {(counter === 4) &&<h2> TERMINOS Y <span className='orange'>CONDICIONES</span></h2>}
         </div>
 
         <div className='form-section'>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <p>Queremos saber que eres, por favor ingresa los siguientes datos.</p>
-                <label>Nombres(s)</label>
-                <input type="text" {...register("firstName", { required: true, minLength: 5 })}/> <span className='block'></span>
-                {errors.firstName && <span className='red'>El nombre requiere minimo 5 carácteres</span>}
+                {(counter === 1) && (
+                    <>
+                        <p>Queremos saber que eres, por favor ingresa los siguientes datos.</p>
+                        <label>Nombres(s)</label>
+                        <input type="text" {...register("firstName", { required: true, minLength: 5 })}/> <span className='block'></span>
+                        {errors.firstName && <span className='red'>El nombre requiere minimo 5 carácteres</span>}
 
-                <label>Apellidos(s)</label>
-                <input type="text" {...register("lastName", { required: true })}/> <span className='block'></span>
-                {errors.lastName && <span className='red'>Por favor llena este campo</span>}
-                <input type='submit' value="Enviar" aria-label='Enviar' className='btn-form disabled' onClick={() => setProgress(prevState => prevState + 25)}/>
+                        <label>Apellidos(s)</label>
+                        <input type="text" {...register("lastName", { required: true })}/> <span className='block'></span>
+                        {errors.lastName && <span className='red'>Por favor llena este campo</span>}
+                    </>
+                )}
+
+                {(counter === 2) && (
+                    <>
+                        <p>Queremos saber que eres, por favor ingresa los siguientes datos.</p>
+                    
+                        <label>Número de Celular</label>
+                        <input type="number" {...register("cellphone", { required: true, minLength: 3 })}/> <span className='block'></span>
+                        {errors.cellphone && <span className='red'>Por favor llena este campo</span>}
+                    </>
+                )}
+
+                {(counter === 3) && (
+                    <>
+                        <p>Queremos saber que eres, por favor ingresa los siguientes datos.</p>
+                        <label>Nombres(s)</label>
+                        <input type="text" {...register("firstName", { required: true, minLength: 5 })}/> <span className='block'></span>
+                        {errors.firstName && <span className='red'>El nombre requiere minimo 5 carácteres</span>}
+
+                        <label>Apellidos(s)</label>
+                        <input type="text" {...register("lastName", { required: true })}/> <span className='block'></span>
+                        {errors.lastName && <span className='red'>Por favor llena este campo</span>}
+                    </>
+                )}
+
+                {(counter === 4) && (
+                    <>
+                        <p>Queremos saber que eres, por favor ingresa los siguientes datos.</p>
+                        <label>Nombres(s)</label>
+                        <input type="text" {...register("firstName", { required: true, minLength: 5 })}/> <span className='block'></span>
+                        {errors.firstName && <span className='red'>El nombre requiere minimo 5 carácteres</span>}
+
+                        <label>Apellidos(s)</label>
+                        <input type="text" {...register("lastName", { required: true })}/> <span className='block'></span>
+                        {errors.lastName && <span className='red'>Por favor llena este campo</span>}
+                    </>
+                )}
+                <input type='submit' value="Enviar" aria-label='Enviar' className='btn-form' onClick={() => setProgress(prevState => prevState + 25)
+                   }/>
             </form>
 
             <figure>
