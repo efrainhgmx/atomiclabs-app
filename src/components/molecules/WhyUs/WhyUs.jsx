78b0133a910dd65d2a6d9546de50cb4a0e349493
Efrain Hernandez <efrainhg2050@gmail.com>
2022-04-02 21:55:07 -0600

    Table section done

--- src/components/molecules/WhyUs/WhyUs.jsx
+++ src/components/molecules/WhyUs/WhyUs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./WhyUs.css";
 
 import techIMG from "../../../assets/images/tech.png";
@@ -31,6 +32,39 @@ const WhyUs = () => {
                     <p><img src={checkIMG} alt="check" loading='lazy' width={20} height={20}/>  ¡Tenemos un parque frente a la oficina!</p>
                 </div>
             </div>
+
+            <div className='table'>
+                <h3>CARACTERÍSTICAS</h3>
+                <h3>OTROS</h3>
+                <h3>ATOMIC</h3>
+
+                <p>Equipo inclusivo, honesto y auténtico</p>
+                <p className='check'><img src={checkIMG} alt="check" loading='lazy' width={20} height={20}/></p>
+                <p className='check'><img src={checkIMG} alt="check" loading='lazy' width={20} height={20}/></p>
+
+                <p>Puntualidad es nuestro segundo nombre</p>
+                <p className='check'><img src={checkIMG} alt="check" loading='lazy' width={20} height={20}/></p>
+                <p className='check'><img src={checkIMG} alt="check" loading='lazy' width={20} height={20}/></p>
+
+                <p>Siempre innovamos en nuestros productos</p>
+                <p className='check'><img src={checkIMG} alt="check" loading='lazy' width={20} height={20}/></p>
+                <p className='check'><img src={checkIMG} alt="check" loading='lazy' width={20} height={20}/></p>
+
+                <p>Te ayudamos a crecer e implementar nuevos conocimientos</p>
+                <p></p>
+                <p className='check'><img src={checkIMG} alt="check" loading='lazy' width={20} height={20}/></p>
+
+                <p>Nos preocupamos por tu bienestar</p>
+                <p></p>
+                <p className='check'><img src={checkIMG} alt="check" loading='lazy' width={20} height={20}/></p>
+
+                <p>El respeto es algo fundamental</p>
+                <p></p>
+                <p className='check'><img src={checkIMG} alt="check" loading='lazy' width={20} height={20}/></p>
+            </div>
+
+            
+            <Link className='btn-primary' to="/" rel='noopener noreferrer'>¡Quiero ser parte!</Link> 
         </div>
     </section>
   )
