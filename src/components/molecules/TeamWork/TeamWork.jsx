dbd8d11617ad13672329a1c03d58e1cace416175
Efrain Hernandez <efrainhg2050@gmail.com>
2022-04-02 20:42:23 -0600

    Work team section done

--- src/components/molecules/TeamWork/TeamWork.jsx
+++ src/components/molecules/TeamWork/TeamWork.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import "./TeamWork.css";
+
+import teamworkIMG from "../../../assets/images/teamwork.png";
 
 const TeamWork = () => {
   return (
-    <div>TeamWork</div>
+    <section className='TeamWork'>
+        <h2>¡TE ENCATARÁ <span className='orange'>TRABAJAR CON NOSOTROS!</span></h2>
+        <div className='TeamWork-content'>
+            <img src={teamworkIMG} alt="Trabaja con nosotros" loading='lazy'/>
+            <div className='TeamWork-content__text'>
+                <p>Contratación remota  <span className='orange'>&rarr;</span></p>
+                <p>Entrevista con area de RH  <span className='orange'>&rarr;</span></p>
+                <p>Prueba práctica  <span className='orange'>&rarr;</span></p>
+                <p>Entrevista Técnica</p>
+            </div>
+            <Link className='btn-primary' to="/" rel='noopener noreferrer'>¡Quiero ser parte!</Link> 
+        </div>
+    </section>
   )
 }
 
