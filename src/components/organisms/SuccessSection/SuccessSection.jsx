85e3995655aa8f489a5e7c368a14d6f14f88530f
Efrain Hernandez <efrainhg2050@gmail.com>
2022-04-03 20:11:47 -0500

    style correction in success section

--- src/components/organisms/SuccessSection/SuccessSection.jsx
+++ src/components/organisms/SuccessSection/SuccessSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import developerIMG from "../../../assets/images/developer.png";
 import "./SuccessSection.css";
 
@@ -12,6 +13,8 @@ const SuccessSection = () => {
         <p>En breve recibiras un correó de confirmación por parte del equipo de AtomicLabs.</p>
         <p>Recuerda revisar tu carpeta de spam</p>
         <p>¡Esperamos verte pronto!</p>
+
+        <Link className='btn-secundary' to="/" rel='noopener noreferrer'>Volver al inicio</Link>
       </div>
 
       <figure>
